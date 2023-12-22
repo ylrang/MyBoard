9f01db395119ded1bb6c80cf98182f2b2cb37fb3
ylrang <yl5649@ncsquare.co.kr>
2023-12-22 16:05:54 +0900

    modified

--- cloud/forms.py
+++ cloud/forms.py
@@ -4,9 +4,9 @@ from .models import Post, Document
 
 
 class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content',  'author', 'category', 'institute']
+        class Meta:
+                model = Post
+                fields = ['title', 'content',  'author', 'category', 'institute']
         
 class DocumentForm(forms.ModelForm):
         files = forms.FileField(label='첨부 파일', required=False, widget=forms.FileInput(attrs={'class': 'form'}))
