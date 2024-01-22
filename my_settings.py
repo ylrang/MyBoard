1020e501db1a984a3b4bbf45c094c3ad721a7e46
ylrang <yl5649@ncsquare.co.kr>
2024-01-22 13:07:20 +0900

    modified_v2

---
+++ my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_board',
+        'USER': 'root',
+        'PASSWORD': 'devncnc2023N',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
