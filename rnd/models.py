bb88f5d8c6e792d9ebe6192046a5fb91a28035bc
ylrang <yl5649@ncsquare.co.kr>
2024-02-05 11:27:40 +0900

    24/02/05

--- rnd/models.py
+++ rnd/models.py
@@ -28,8 +28,3 @@ class Case(models.Model):
     def __str__(self):
         return self.title
         
-    
-class Comment(models.Model):
-    case = models.ForeignKey(Case, on_delete=models.CASCADE)
-    content = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
